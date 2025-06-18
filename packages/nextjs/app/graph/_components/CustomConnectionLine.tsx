5f9154ed5c309d9925be2cc6baa640fb5655a9ca
Marcus Rein <marcus.rein@gmail.com>
2025-06-18 11:16:55 -0400

    feat: increase marker size for custom connection lines in graph component

--- packages/nextjs/app/graph/_components/CustomConnectionLine.tsx
+++ packages/nextjs/app/graph/_components/CustomConnectionLine.tsx
@@ -17,8 +17,8 @@ const CustomConnectionLine: React.FC<ConnectionLineComponentProps> = ({ fromX, f
           viewBox="0 0 10 10"
           refX="10"
           refY="5"
-          markerWidth="6"
-          markerHeight="6"
+          markerWidth="24"
+          markerHeight="24"
           orient="auto-start-reverse"
         >
           <path d="M 0 0 L 10 5 L 0 10 z" fill="#f59e0b" />
